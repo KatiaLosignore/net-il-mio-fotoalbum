04ae16a6881722c36f4e7e91ed928171b5b64200
KatiaLosignore <katialos.kl@gmail.com>
2023-10-12 20:46:39 +0200

    added filter photo in Index page admin

---
+++ net-il-mio-fotoalbum/Controllers/Api/PhotosController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models.Database_Models;
+
+namespace net_il_mio_fotoalbum.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+
+        // uso la  Dependency Injection
+
+        private IRepositoryPhotos _repoPhotos;
+
+        public PhotosController(IRepositoryPhotos repoPhotos)
+        {
+            _repoPhotos = repoPhotos;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetPhotos()
+        {
+
+            List<Photo> photos = _repoPhotos.GetPhotos();
+
+            return Ok(photos);
+
+        }
+
+
+        [HttpGet]
+        public IActionResult SearchPhoto(string? search)
+        {
+            List<Photo> foundedPhotos = new List<Photo>();
+
+            if (search == null)
+            {
+                foundedPhotos = _repoPhotos.GetPhotos();
+            }
+            else
+            {
+                foundedPhotos = _repoPhotos.GetPhotosByTitle(search);
+            }
+
+            return Ok(foundedPhotos);
+
+        }
+
+    }
+}
