88db07488088a6c7f90a8f44ad1d4ced098a439d
KatiaLosignore <katialos.kl@gmail.com>
2023-10-13 12:03:59 +0200

    added class message

---
+++ net-il-mio-fotoalbum/Controllers/Api/MessageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using net_il_mio_fotoalbum.Database;
+
+namespace net_il_mio_fotoalbum.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private FotoalbumContext _db;
+
+        public MessageController(FotoalbumContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage([FromBody] Message message)
+        {
+            try
+            {
+                _db.Messages.Add(message);
+                _db.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+        }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Database Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models.Database_Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "L'email è obbligatoria")]
+        [EmailAddress(ErrorMessage = "Formato dell'email non valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Il messaggio da inserire è obbligatorio")]
+        public string MessageText { get; set; }
+
+        // COSTRUTTORE
+        public Message()
+        {
+
+        }
+    }
+}
